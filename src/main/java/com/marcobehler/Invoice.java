28f1c03afae0e0c619b19e65cd22fc35a1c85d7a
Christian <christian.alos.segui@gmail.com>
2022-08-01 17:58:13 +0200

    initial commit

---
+++ src/main/java/com/marcobehler/Invoice.java
@@ -0,0 +1,46 @@
+package com.marcobehler;
+import java.util.UUID;
+
+public class Invoice {
+
+    private String id, userId, pdfUrl;
+    private Integer amount;
+
+    // constructor por defecto
+    public Invoice() {
+    }
+
+    // constructor
+    public Invoice(String userId, Integer amount, String pdfUrl) {
+        this.id = UUID.randomUUID().toString();
+        this.userId = userId;
+        this.pdfUrl = pdfUrl;
+        this.amount = amount;
+    }
+
+    // getters and setters
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getUserId() {
+        return userId;
+    }
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+    public String getPdfUrl() {
+        return pdfUrl;
+    }
+    public void setPdfUrl(String pdfUrl) {
+        this.pdfUrl = pdfUrl;
+    }
+    public Integer getAmount() {
+        return amount;
+    }
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+}
